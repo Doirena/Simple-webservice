42b6974f5b3fdc1265391e8c619e3bbddca98cf5
Doreina <barkauskaite.dovile@gmail.com>
2020-08-30 18:49:47 +0300

    create a project

---
+++ src/test/java/com/dovile/simplewebservice/SimpleWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dovile.simplewebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
