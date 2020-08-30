fd6c4b2aeb9e3f627a49e3911d48669a84ea9578
Doreina <barkauskaite.dovile@gmail.com>
2020-08-30 21:35:32 +0300

    add domain package and Customer class for request body

---
+++ src/main/java/com/dovile/simplewebservice/domain/Customer.java
@@ -0,0 +1,21 @@
+package com.dovile.simplewebservice.domain;
+
+/**
+ *
+ * @author Dovile Barkauskaite <barkauskaite.dovile@gmail.com>
+ */
+public class Customer {
+    private String first_name;
+    private String last_name;
+
+    public Customer() {
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+}
