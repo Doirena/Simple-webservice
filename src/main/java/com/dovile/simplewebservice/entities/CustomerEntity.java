451c74707f957ca50f8658e0a0f78406e6e7d7e0
Doreina <barkauskaite.dovile@gmail.com>
2020-08-30 21:34:02 +0300

    correct CustomerEntity class

--- src/main/java/com/dovile/simplewebservice/entities/CustomerEntity.java
+++ src/main/java/com/dovile/simplewebservice/entities/CustomerEntity.java
@@ -1,4 +1,55 @@
 package com.dovile.simplewebservice.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GenerationType;
+
+/**
+ *
+ * @author Dovile Barkauskaite <barkauskaite.dovile@gmail.com>
+ */
+@Entity
+@Table(name = "customer")
 public class CustomerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String first_name;
+    private String last_name;
+
+    public CustomerEntity() {
+    }
+
+    public CustomerEntity(Integer id, String first_name, String last_name) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
 }
