7181dc84d4d45d7b03b56874cfa99252d2a79a16
Doreina <barkauskaite.dovile@gmail.com>
2020-08-30 21:32:27 +0300

    add exception package, where can throw ErrorDetails class message for clientserviceimpl methods

---
+++ src/main/java/com/dovile/simplewebservice/exception/ErrorDetails.java
@@ -0,0 +1,32 @@
+package com.dovile.simplewebservice.exception;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Dovile Barkauskaite <barkauskaite.dovile@gmail.com>
+ */
+public class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public ErrorDetails(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+}
