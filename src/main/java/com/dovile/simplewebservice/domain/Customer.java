052f6f973fa142f94d617db1c47c5c5ceca3deaf
Doreina <barkauskaite.dovile@gmail.com>
2020-08-30 23:40:49 +0300

    Customer class add constructor

--- src/main/java/com/dovile/simplewebservice/domain/Customer.java
+++ src/main/java/com/dovile/simplewebservice/domain/Customer.java
@@ -11,6 +11,11 @@ public class Customer {
     public Customer() {
     }
 
+    public Customer(String first_name, String last_name) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
     public String getFirst_name() {
         return first_name;
     }
