424ea44c1f7bc1f515a5c699501eafe400f27ff0
Doreina <barkauskaite.dovile@gmail.com>
2020-08-30 21:30:39 +0300

    add in to database some value

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO customer (first_name, last_name) VALUES ('Anna', 'Smith');
+INSERT INTO customer (first_name, last_name) VALUES ('John', 'Depp');
+INSERT INTO customer (first_name, last_name) VALUES ('Victoria', 'Coleman');
+INSERT INTO customer (first_name, last_name) VALUES ('Tom', 'Hughes');
