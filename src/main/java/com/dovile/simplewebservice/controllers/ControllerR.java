24a0660a6cb67ef6e828659c635748038113466e
Doreina <barkauskaite.dovile@gmail.com>
2020-08-30 21:35:47 +0300

    Controller for rest api

---
+++ src/main/java/com/dovile/simplewebservice/controllers/ControllerR.java
@@ -0,0 +1,55 @@
+package com.dovile.simplewebservice.controllers;
+
+import com.dovile.simplewebservice.domain.Customer;
+import com.dovile.simplewebservice.entities.CustomerEntity;
+import com.dovile.simplewebservice.exception.ResourceNotFoundException;
+import com.dovile.simplewebservice.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Dovile Barkauskaite <barkauskaite.dovile@gmail.com>
+ */
+@RestController
+@RequestMapping("/api")
+public class ControllerR {
+
+    @Autowired
+    CustomerService customerService;
+
+    @GetMapping("/customer")
+    public List<CustomerEntity> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping("/customer/{id}")
+    public CustomerEntity getCustomerById(@PathVariable(value = "id") Integer id)
+            throws ResourceNotFoundException {
+        return customerService.getCustomer(id);
+    }
+
+    @PostMapping("/customer")
+    public CustomerEntity createCustomer(@RequestBody @Valid Customer customer) {
+        return customerService.createCustomer(customer);
+    }
+
+    @PutMapping("/customer/{id}")
+    public ResponseEntity<CustomerEntity> updateCustomer(
+            @PathVariable(value = "id") Integer id,
+            @RequestBody @Valid Customer customer)
+            throws ResourceNotFoundException {
+        return customerService.updateCustomer(customer, id);
+    }
+
+    @DeleteMapping("/customer/{id}")
+    public Map<String, Boolean> deleteOwner(@PathVariable(value = "id") Integer id)
+            throws Exception {
+        return customerService.deleteCustomer(id);
+    }
+}
