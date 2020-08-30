2728c34713999f41b3d48fd69dc8568f5a3a0ca2
Doreina <barkauskaite.dovile@gmail.com>
2020-08-30 21:33:41 +0300

    add  basic config SecurityConfiguration class, where is username and password also role for path

---
+++ src/main/java/com/dovile/simplewebservice/securityconfig/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.dovile.simplewebservice.securityconfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ *
+ * @author Dovile Barkauskaite <barkauskaite.dovile@gmail.com>
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}dovile").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                //HTTP Basic authentication
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/customer/**").hasRole("USER")
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+    }
+}
