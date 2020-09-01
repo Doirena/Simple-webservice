510b6fc0b6f52da2786c9102897bb0f3f78b9de4
Doreina <barkauskaite.dovile@gmail.com>
2020-09-01 23:47:47 +0300

    create CustomerEndpoint class with two methods get and upadate customers

---
+++ src/main/java/com/dovile/simplewebservice/endpoints/CustomerEndpoint.java
@@ -0,0 +1,51 @@
+package com.dovile.simplewebservice.endpoints;
+
+import com.dovile.simple_web_service.CustomerInfo;
+import com.dovile.simple_web_service.GetAllCustomersResponse;
+import com.dovile.simple_web_service.UpdateCustomerRequest;
+import com.dovile.simple_web_service.UpdateCustomerResponse;
+import com.dovile.simplewebservice.domain.Customer;
+import com.dovile.simplewebservice.exception.ResourceNotFoundException;
+import com.dovile.simplewebservice.mapper.CustomerInfoMapper;
+import com.dovile.simplewebservice.service.CustomerService;
+import lombok.AllArgsConstructor;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Endpoint
+@AllArgsConstructor
+public class CustomerEndpoint {
+    private static final String NAMESPACE_URI = "http://dovile.com/simple-web-service";
+    private CustomerService customerService;
+    private CustomerInfoMapper customerInfoMapper;
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getAllCustomersRequest")
+    @ResponsePayload
+    public GetAllCustomersResponse getAllArticles() {
+        GetAllCustomersResponse response = new GetAllCustomersResponse();
+
+        List<CustomerInfo> customerInfoList=customerService.getAllCustomers().stream()
+                .map(customerInfoMapper::mapToCustomerResponse)
+                .collect(Collectors.toList());
+
+        response.getCustomerInfo().addAll(customerInfoList);
+        return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "updateCustomerRequest")
+    @ResponsePayload
+    public UpdateCustomerResponse updateCustomer(@RequestPayload UpdateCustomerRequest request) throws ResourceNotFoundException {
+        Customer customer= new Customer(request.getFirstName(), request.getLastName());
+        CustomerInfo customerInfo = customerInfoMapper
+                .mapToCustomerResponse(customerService.updateCustomer(customer,request.getCustomerId()));
+
+        UpdateCustomerResponse response = new UpdateCustomerResponse();
+        response.setCustomerInfo(customerInfo);
+        return response;
+    }
+}
