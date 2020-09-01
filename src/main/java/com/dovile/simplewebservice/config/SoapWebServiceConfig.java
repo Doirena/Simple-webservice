2ec06eea3772ac72d6f8fbf9f9a51361488f220b
Doreina <barkauskaite.dovile@gmail.com>
2020-09-02 00:59:05 +0300

    add configuration for soap security. Add user name and password, make Callback Handler and Security Policy methods which return it is true or false, if it is true return Response if not Client get message.

--- src/main/java/com/dovile/simplewebservice/config/SoapWebServiceConfig.java
+++ src/main/java/com/dovile/simplewebservice/config/SoapWebServiceConfig.java
@@ -7,14 +7,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.xwss.XwsSecurityInterceptor;
+import org.springframework.ws.soap.security.xwss.callback.SimplePasswordValidationCallbackHandler;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
+import java.util.Collections;
+import java.util.List;
+
 @Configuration
 @EnableWs
 public class SoapWebServiceConfig extends WsConfigurerAdapter {
+
     @Bean
     public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
         MessageDispatcherServlet servlet = new MessageDispatcherServlet();
@@ -36,5 +43,29 @@ public class SoapWebServiceConfig extends WsConfigurerAdapter {
         wsdl11Definition.setTargetNamespace("http://dovile.com/simple-web-service");
         wsdl11Definition.setSchema(customersSchema);
         return wsdl11Definition;
+    }
+
+    //XwsSecurityInterceptor
+    @Bean
+    public XwsSecurityInterceptor securityInterceptor(){
+        XwsSecurityInterceptor securityInterceptor = new XwsSecurityInterceptor();
+        //Callback Handler -> SimplePasswordValidationCallbackHandler
+        securityInterceptor.setCallbackHandler(callbackHandler());
+        //Security Policy -> securityPolicy.xml
+        securityInterceptor.setPolicyConfiguration(new ClassPathResource("securityPolicy.xml"));
+        return securityInterceptor;
+    }
+
+    @Bean
+    public SimplePasswordValidationCallbackHandler callbackHandler() {
+        SimplePasswordValidationCallbackHandler handler = new SimplePasswordValidationCallbackHandler();
+        handler.setUsersMap(Collections.singletonMap("user", "password"));
+        return handler;
+    }
+
+    //Interceptors.add -> XwsSecurityInterceptor
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        interceptors.add(securityInterceptor());
     }
 }
